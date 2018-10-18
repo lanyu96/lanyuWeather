cc2a766f9d21e036a23bb23a600d56902c766402
lanyu96 <961101679@qq.com>
2018-10-18 10:15:23 +0800

    update

--- app/src/main/java/com/lanyuweather/zhou/WeatherActivity.java
+++ app/src/main/java/com/lanyuweather/zhou/WeatherActivity.java
@@ -10,6 +10,8 @@ package com.lanyuweather.zhou;
 //import android.widget.TextView;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.view.GravityCompat;
@@ -54,7 +56,14 @@ public class WeatherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+        //实现背景图和状态栏融合到一起的效果
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+
+        }
 
 
         setContentView(R.layout.activity_weather);
